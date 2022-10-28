59152de98a7e16d693142ffa6b18ac1e8709677e
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-10-28 15:27:00 +0600

    register component added

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,11 +1,54 @@
- import React from 'react';
+ 
+import { Button, Form } from 'react-bootstrap';
  
  const Register = () => {
+
+    
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, photoURL, email, password);
+
+       
+        
+    }
     return (
-        <div>
-            <h2>This is Register</h2>
-        </div>
+        <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Your Name</Form.Label>
+            <Form.Control type="text" name="name" placeholder="Enter your name" />
+           
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Photo URL</Form.Label>
+            <Form.Control type="text" name="photoURL" placeholder="photo url" />
+           
+        </Form.Group>
+            
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" name="email" placeholder="Enter email" required/>
+           
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" name="password" placeholder="Password" required />
+        </Form.Group>
+        
+        <Button variant="primary" type="submit">
+             Sign Up
+        </Button>
+        <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+        </Form.Text>
+      </Form>
     );
- };
- 
- export default Register;
+};
+
+export default Register;
